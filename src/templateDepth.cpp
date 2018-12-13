5106f2c6b2d77c270e93315b82863af4778fa013
zeshan <nzeshanalide@gmail.com>
2018-12-13 20:21:39 +0100

    Initial commit

---
+++ src/templateDepth.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : templateDepth.cpp
+// Author      : Zeshan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "!!!Pattern  Testing !!!" << endl; // prints !!!Pattern  Testing !!!
+	return 0;
+}
